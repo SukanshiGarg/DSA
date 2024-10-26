81bfcbdb70e15f53397f62fdcda4b376c382940e
SukanshiGarg <sukanshigargagra123@gmail.com>
2024-10-26 12:45:54 +0530

    first commit

--- Searching/BinarySearch.java
+++ Searching/BinarySearch.java
@@ -1,5 +1,6 @@
 package Searching;
 //binary search happens only on sorted arrays 
+//binary search is always applied on sorted search space.
 public class BinarySearch {
     public static void main(String[] args) {
         int arr[] = {1, 2, 3, 4, 5, 6, 7};

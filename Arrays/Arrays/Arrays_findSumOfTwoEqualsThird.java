0e5c58ceefe31a0bbfab75ad20100e310728e378
SukanshiGarg <sukanshigargagra123@gmail.com>
2024-10-27 17:35:27 +0530

    first commit

---
+++ Arrays/Arrays/Arrays_findSumOfTwoEqualsThird.java
@@ -0,0 +1,26 @@
+package Arrays;
+import java.util.HashSet;
+public class Arrays_findSumOfTwoEqualsThird {
+    public static boolean solution(int arr[]){
+        HashSet<Integer> set= new HashSet<>();
+        for(int num:arr){
+            set.add(num);
+        }
+        int n=arr.length;
+        for(int i=0;i<n;i++){
+            for(int j=i+1;j<n;j++){
+                int sum=arr[i]+arr[j];
+                //check if the sum exists in the set and is not arr[i] and arr[j]
+                if(set.contains(sum) && sum!=arr[i] && sum!=arr[j]){
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+    public static void main(String[] args) {
+        int arr[]={1,2,3,4,5,6};
+        System.out.println(solution(arr));
+    }
+    
+}

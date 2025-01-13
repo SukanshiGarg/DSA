00e3b21ebf8654f75017caf2ab31fe3354e754bf
SukanshiGarg <sukanshigargagra123@gmail.com>
2025-01-13 22:11:48 +0530

    first commit

---
+++ RECURSION/dice.java
@@ -0,0 +1,22 @@
+package RECURSION;
+
+public class dice {
+    public static void main(String[] args) {
+        int n=4;
+        printans(n,0,"");
+    }
+
+    public static void printans(int n, int curr, String ans){
+        if(curr == n ) {
+            System.out.println(ans+" ");
+            return;
+        }
+        if(curr>n){
+            return;
+        }
+        for(int dice=1;dice<=3;dice++){
+            printans(n, curr+dice, ans+dice);
+        }
+    }
+    
+}
